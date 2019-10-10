d00a52367edab0a7613982cd63a1364e1bbcd3b8
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2019-10-09 20:23:01 -0500

    Added google analytics tag

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   title: 'Manguito Blue - Blog',
+  ga: 'UA-149793240-1',
   locales: {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
